3e2ffabd84ffdc762c496d38972a69d57768b163
omkar <omkarsp20@gmail.com>
2026-07-25 11:34:46 +0530

    Practice Mini Project

---
+++ Day_02/mini_project.py
@@ -0,0 +1,27 @@
+"""
+Mini Project
+
+Project Name: Personal Profile Card
+
+Objective: Create a simple Python program that stores and displays personal information.
+
+Functional Requirements:
+
+Store your name, age, city, favorite programming language, and a constant such as COUNTRY.
+Print the information in a neat format.
+Display the data type of at least two variables using type().
+Display the object identity of one variable using id().
+Use meaningful variable names, proper indentation, and comments where helpful.
+"""
+name = "Amruta"
+age = 24
+city = "Pune"
+favourite_language = "JavaScript"
+COUNTRY = "India"
+print("Name :",name)
+print("Age :",age)
+print("City :",city)
+print("Favourite Programming Language :",favourite_language)
+print(type(age))
+print(type(favourite_language))
+print("Id =",id(age))
