3fd52fd591da13932e630a07765af553017027ef
omkar <omkarsp20@gmail.com>
2026-07-30 12:04:17 +0530

    Explore Conceptual Questions

---
+++ Day_04/conceptual_question.py
@@ -0,0 +1,33 @@
+# 1. What is the difference between an operator and an operand?
+"""
+1.operator is a special symbol which used to perform the operation on operands , values , variables.
+2.Operators are  +,-,*,/,//,**,%,= etc....
+3.Operand is a actual values work with operators.
+4.Example 10 + 2 so here + is operator and 10 or 2 are the values we called them as an operand.
+"""
+
+# 2. What is the difference between = and ==?
+"""
+1. = is an assignment operator which is used to assign the RHS value to the LHS of variable.
+2. == is an equality operator which used to check whether both LHS or RHS are equals or not.
+"""
+
+# 3. What is the difference between == and is?
+"""
+1. == is an equality operator which used to check whether both LHS or RHS are equals or not.
+2. is is an identity operaror which used to check whether both objects are point to the same memory location or not.
+3.== only check the value but is is used to check the memory address.
+"""
+
+# 4. What is short-circuit evaluation?
+"""
+1.short-circuit evaluation means while evaluating the expression python already know the result.
+2.So instead of checking remaing condition it stop and generate the result.
+3.Example : print((10 > 5) or (10 == 10)) --> Here (10 > 5) after this condition there is or operator so instead of checking other condition it return True.
+"""
+
+# 5. Why are parentheses useful in expressions?
+"""
+1.paranthesis useful in expression because we can change the evaluation based on our requirement.
+2.Paranthesis also makes the code much readable or easier to understand.
+"""
