63ca08b3f1b1d8d3a245d836d98f0989f0821b91
omkar <omkarsp20@gmail.com>
2026-07-30 11:59:39 +0530

    Explore Operators

---
+++ Day_04/operators.py
@@ -0,0 +1,59 @@
+# Operators :
+"""
+Operators are special symbol which used to perform the operation on operands or values.
+Example : 3 + 4 = 7  Here 3 or 4 are the operands and + is an operator
+
+There are different types of operator in python :
+    1.Arithmatic operators :
+            1.Arithmatic operator is used to perform the mathmatical calculations on operands or values or variables.
+            2.Arithmatic operators are :
+                    1.Addition[ + ]
+                    2.Subtraction [ - ]
+                    3.Multiplication [ * ]
+                    4.Division [ / ]
+                    5.Modulus [ % ]
+                    6.Floor division [ // ]
+                    7.Exponent [ ** ]
+
+    2.Relational / comparision operators :
+            1.Comparison operators are used to compare two values.
+            2.Comparison operators are always return a boolean value as result.
+            3.Comparison operators are :
+                    1.Equals to [ = ]
+                    2.Not equals to [ != ]
+                    3.Greater than [ > ]
+                    4.Less than [ < ]
+                    5.Greater than or Equals [ >= ]
+                    6.Less than or equals [ <= ]
+
+    3.Logical operators :
+            1.When we want to check the multiple conditions then we used logical operators.
+            2.Logical operators are :
+                    1.and
+                    2.or
+                    3.not
+
+    4.Assignment operators :
+            1.Assignment operators are used to assign the values to variables.
+            2.Assignment operators are used to evaluates the expression on RHS and then assign the result to LHS of variable.
+            3.Assignment operators are :
+                    1.Equals to [ == ]
+                    2.Addition assignment [ += ]
+                    3.Subtraction assignment [ -= ]
+                    4.Multiplication assignment [ *= ]
+                    5.Division assignment [ /= ]
+                    6.Floor division assignement [ //= ]
+                    7.Exponent assignment [ **= ]
+
+    5.Membership operators :
+            1.Membership operators are used to check whether the object occurs in another object.
+            2.Membership operators are :
+                    1.in
+                    2.not in
+
+    6.Identity operators :
+            1.Identity operators are used to check whether both the objects are points to the same memory location or not.
+            2.Identity operators are :
+                    1.is
+                    2.is not
+"""
