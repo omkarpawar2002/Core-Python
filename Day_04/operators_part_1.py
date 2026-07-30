383e3e538796d0fa7ef8d683b1b4db88d2a2571b
omkar <omkarsp20@gmail.com>
2026-07-30 12:02:58 +0530

    Explore Arithmatic or Comparison Operators

---
+++ Day_04/operators_part_1.py
@@ -0,0 +1,131 @@
+# 1.Arithmatic operators :
+
+"""
+1.Addition [ + ] :
+        Addition is used to add 2 numbers.
+"""
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+print(f"Addition of {n1} + {n2} = {n1 + n2}")
+
+"""
+1.Subtraction [ - ] :
+        Addition is used to subtract second number from first number.
+"""
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+print(f"Subtraction of {n1} - {n2} = {n1 - n2}")
+
+"""
+3.Multiplication [ * ] :
+        Multiplication is used to multiply 2 numbers.
+"""
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+print(f"Multiplication of {n1} * {n2} = {n1 * n2}")
+
+"""
+4.Division [ / ] :
+        Division is used to divide 2 numbers and return a float[quotient] value.
+"""
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+print(f"Division of {n1} / {n2} = {n1 / n2}")
+
+"""
+5.Floor Division [ // ] :
+        Floor Division is used to divide 2 numbers and return rounded backword integer quotient value . 
+        So decimal part truncated.
+"""
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+print(f"Floor Division of {n1} // {n2} = {n1 // n2}")
+
+"""
+6.Modulus [ % ] :
+        Modulus operator is used to return remainder.
+"""
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+print(f"Modulus of {n1} % {n2} = {n1 % n2}")
+
+"""
+7.Exponent [ ** ] :
+        Exponent operator is used to return the power of value.
+"""
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+print(f"Exponent of {n1} ** {n2} = {n1**n2}")
+
+print(
+    "=================================================================================================="
+)
+
+# 2.Comparison operators :
+
+"""
+1.Equals to [ == ] :
+        1.Equals to operators are used to check whether LHS or RHS both are equals.
+        2.It return True if both are equal or return False if both are not equals.
+"""
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+print(n1 == n2)
+
+"""
+2.Not Equals to [ != ] :
+        1.Not Equals to operator are used to check whether LHS or RHS both are not equals.
+        2.It return True if both are not equal or return False if both are equals.
+"""
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+print(n1 != n2)
+
+"""
+3.Greater than [ > ] :
+        1.Greater than operator are used to check whether LHS is greater than RHS.
+        2.It return True if LHS is greater than RHS otherwise it return False.
+"""
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+print(n1 > n2)
+
+"""
+4.Less than [ < ] :
+        1.less than operator are used to check whether LHS is less than RHS.
+        2.It return True if LHS is less than RHS otherwise it return False.
+"""
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+print(n1 < n2)
+
+"""
+5.Greater than Equals to [ >= ] :
+        1.Greater than or equaals to operator are used to check whether LHS is greater than or equals to RHS.
+        2.It return True if LHS is greater than or equals to RHS otherwise it return False.
+"""
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+print(n1 >= n2)
+
+"""
+6.Less than Equals to [ <= ] :
+        1.Less than or equaals to operator are used to check whether LHS is less than or equals to RHS.
+        2.It return True if LHS is less than or equals to RHS otherwise it return False.
+"""
+
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter second number : "))
+print(n1 <= n2)
