a3016dbba7542c70ce09be63079f0ae5f977756e
omkar <omkarsp20@gmail.com>
2026-08-10 12:14:19 +0530

    Practice Some Conceptual Quetions

---
+++ Day_07/conceptual_questions.py
@@ -0,0 +1,60 @@
+# What is dynamic typing in Python?
+"""
+1.Python is a dynamically type programming language.
+2.Here we no need to define the data type while declaring a variable.
+3.During runtime python will automatically find out which type of variable is this.
+"""
+
+# What is the difference between = and ==?
+"""
+1.= is an assignment operator which is responsible for assigning the right hand side value to the left hand side of variable.
+2.== is an equality operator which check whether both LHS or RHS are equal or not.
+"""
+
+# Why is indentation important in Python?
+"""
+1.In other programming language to define a block of code we used curly brackets {}.
+2.So here in python there is no concept of curly brackets here we used indentation to define a block.
+"""
+
+# What is the purpose of type()?
+"""
+type() function is used to return the data type of an object.
+"""
+
+# What is the difference between implicit and explicit type conversion?
+"""
+Main difference between implicit and explicit type conversion is :
+    1.In implicit type conversion python automatically convert the data type
+    2.In explicit type conversion user manually convert one data type into another by using the built in function like int() , float() and many more.
+"""
+
+# When should you use a for loop instead of a while loop?
+"""
+1.when we know the exact number of time the iteration is required then we used for loop.
+2.when we don't know the exact number of time the iteration is required then we used while loop.
+"""
+
+# What is the difference between break and continue?
+"""
+1.break keyword is used when we immediately want to exits from the loop.
+2.continue keyword is used when we want to skip the current iteration and jump on the next iteration.
+"""
+
+# Why is the stop value excluded in range()?
+"""
+In Python, range() excludes the stop value because it follows the half-open interval [start, stop). 
+This makes loops predictable, avoids off-by-one errors, and matches slicing behavior.
+"""
+
+# What is the purpose of the loop else block?
+"""
+1.loop else block we generally used when we want some condition check like found or not found like this.
+2.else block in loop will get executed only if loop normally run successfully.
+3.or if the loop break forcefully by using break keyword then else not run.
+"""
+
+# What is short-circuit evaluation?
+"""
+short-circuit evaluation means when python already know the result so instead of checking other condition it stop.
+"""
